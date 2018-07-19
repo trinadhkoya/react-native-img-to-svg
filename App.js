f7f825b7a228266410b5be75edab7847cb1db2a8
Trinadh Koya <trinadh.koya@zaggle.in>
2018-07-19 10:14:18 +0530

    changes to svg

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import SupportIco from "./assets/supportIco";
 import ZcardIco from "./assets/zCardIco";
 import DesertIco from "./assets/desertIco";
 import GiftCardIco from "./assets/giftCardIco";
+import GiftCardIco2 from "./assets/giftCardIco2";
 
 
 const instructions = Platform.select({
@@ -32,6 +33,12 @@ export default class App extends Component<Props> {
     render() {
         return (
             <ScrollView>
+
+                <GiftCardIco2 width={150} height={100} fill={'#6e56a4'} stroke={2} viewBox={'0 0 58 58'}
+                              lightColor={'#6e56a4'}/>
+                <ShoppinCartIco width={150} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
+                                lightColor={'#f48990'}/>
+
                 <BookingIco width={100} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
                             lightColor={'#f48990'}/>
                 <Buffet width={150} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
@@ -43,14 +50,15 @@ export default class App extends Component<Props> {
                 <ComboIco width={150} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
                           lightColor={'#f48990'}/>
                 <GiftCardIco width={150} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
-                             lightColor={'#f48990'}/>  <ShoppinCartIco width={150} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
-                                lightColor={'#f48990'}/>
+                             lightColor={'#f48990'}/> <ShoppinCartIco width={150} height={100} fill={'#eb3a42'}
+                                                                      stroke={2} viewBox={'0 0 58 58'}
+                                                                      lightColor={'#f48990'}/>
                 <SupportIco width={150} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
                             lightColor={'#f48990'}/>
                 <ZcardIco width={150} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
-                            lightColor={'#f48990'}/>
-                <DesertIco width={150} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
                           lightColor={'#f48990'}/>
+                <DesertIco width={150} height={100} fill={'#eb3a42'} stroke={2} viewBox={'0 0 58 58'}
+                           lightColor={'#f48990'}/>
 
             </ScrollView>
         );
